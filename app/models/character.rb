797864fc9c91504059be2fd4bd493a8060809751
AlexandreMrl <mieral.alexandre@gmail.com>
2024-02-29 10:17:40 +0100

    --wip-- [skip ci]

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,11 @@ class Character < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_synopsis,
+                  against: %i[name univers description],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
